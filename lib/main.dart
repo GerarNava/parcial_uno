668aebc6946809949d9ea601e3b5f27f9f25820b
GerarNava <110059931+GerarNava@users.noreply.github.com>
2023-08-24 21:22:55 -0600

    Modificaciones primer parcial

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Inicio de Sesión',
       theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xFF41818A),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 52, 121, 52),
         textTheme: const TextTheme(bodyLarge: TextStyle(color: Colors.white)),
         primarySwatch: Colors.green,
       ),
@@ -37,8 +37,8 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Inicio de Sesión'),
-      ),
+          title: const Text('Inicio de Sesión'),
+          titleTextStyle: const TextStyle(color: Colors.white, fontSize: 25.0)),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
@@ -47,14 +47,15 @@ class _LoginPageState extends State<LoginPage> {
             TextField(
               controller: _emailController,
               decoration: const InputDecoration(
-                labelText: 'Correo Electrónico',
-              ),
+                  labelText: 'Correo Electrónico',
+                  labelStyle: TextStyle(color: Colors.white, fontSize: 18)),
             ),
             const SizedBox(height: 20.0),
             TextField(
               controller: _passwordController,
               decoration: const InputDecoration(
                 labelText: 'Contraseña',
+                labelStyle: TextStyle(color: Colors.white, fontSize: 18),
               ),
               obscureText: true,
             ),
@@ -63,64 +64,20 @@ class _LoginPageState extends State<LoginPage> {
               onPressed: () {
                 // Aquí agregarás la lógica para verificar las credenciales
               },
-              child: const Text('Iniciar Sesión'),
+              child: const Text(
+                'Iniciar Sesión',
+              ),
             ),
             const SizedBox(height: 10.0),
             TextButton(
-              onPressed: () {
-                // Navegar a la pantalla de registro
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RegisterPage()),
-                );
-              },
-              child: const Text('¿No tienes una cuenta? ¡Regístrate aquí!'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class RegisterPage extends StatelessWidget {
-  const RegisterPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Registro'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Nombre de Usuario',
+              onPressed: () {},
+              child: const Text(
+                '¿No tienes una cuenta? ¡Regístrate aquí!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
               ),
-            ),
-            const SizedBox(height: 20.0),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Correo Electrónico',
-              ),
-            ),
-            const SizedBox(height: 20.0),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Contraseña',
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Lógica para realizar el registro
-              },
-              child: const Text('Registrarse'),
             ),
           ],
         ),
